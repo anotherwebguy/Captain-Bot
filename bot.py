0f89392be2fc5ebdec502ab771d0c0547f4f7163
Mohit <mohitrajputms123@gmail.com>
2021-06-27 11:14:58 +0530

    running on flask

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 import discord
 from discord.ext import commands
-import random,os
+import random
 from decouple import config
+from webserver import keep_alive
+import os
 
 TOKEN = config('DISCORD_TOKEN')
 
@@ -25,67 +27,6 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
-# @client.event
-# async def on_ready():
-#     print("The bot is ready")
-#
-# @client.event
-# async def on_member_join(ctx, member):
-#     print(f"{member} has joined the server.")
-#     await ctx.send(f"Welcome {member.mention} to our server")
-#
-# @client.event
-# async def on_member_remove(ctx, member):
-#     print(f"{member} has left the server.")
-#     await ctx.send(f"{member.mention} has left our server")
-#
-# @client.command()
-# async def ping(ctx):
-#     await ctx.send("Pong!")
-#
-# @client.command(aliases = ['8ball','Test'])
-# async def _8ball(ctx, *, question):
-#     responses = ['It is certain.',
-#                  'Without a doubt',
-#                  'Yes. definitely',
-#                  'Most likely',
-#                  'Signs points to yes.',
-#                  'Reply hazy, try again',
-#                  'Ask again later',
-#                  'Better not tell you now',
-#                  'Cannot Predict now',
-#                  'Concentrate and ask again',
-#                  "Don't count on it",
-#                  "My reply is no.",
-#                  'Outlook is not so good',
-#                  'Very doubtfull',
-#                  'My sources say no.']
-#     await ctx.send(f"Question: {question}\nAnswer: {random.choice(responses)}")
-#
-# @client.command()
-# async def clear(ctx, amount=5):
-#     await ctx.channel.purge(limit=amount)
-#
-# @client.command()
-# async def kick(ctx, member : discord.Member, *, reason=None):
-#     await member.kick(reason = reason)
-#     await ctx.sendO(f"Kicked {member.mention} from the server.")
-#
-# @client.command()
-# async def ban(ctx, member : discord.Member, *, reason=None):
-#     await member.ban(reason = reason)
-#     await ctx.send(f"Banned {member.mention} from the server.")
-#
-# @client.command()
-# async def unban(ctx, *, member):
-#     banned_users = await ctx.guild.bans()
-#     member_name, member_descriminator = member.split('#')
-#     for ban_entry in banned_users:
-#         user = ban_entry.user
-#
-#         if( user.name, user.descriminator) == (member_name, member_descriminator):
-#             await ctx.guild.unban(user)
-#             await ctx.send(f"Unbanned {user.name}#{user.descriminator}")
-#             return
+keep_alive()
 
 client.run(TOKEN)
